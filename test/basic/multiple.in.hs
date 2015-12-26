db167e7cf4c41b48b7a8ebe5b38b910eadc1a720
itchyny <itchyny@hatena.ne.jp>
2015-12-26 17:47:08 +0900

    add tests for multiple blocks of imports

---
+++ test/basic/multiple.in.hs
@@ -0,0 +1,11 @@
+import CA
+import BA
+import AA
+
+import CB
+import AB
+import BB
+
+import CC
+import AC
+import BC
---
+++ test/basic/multiple.out.hs
@@ -0,0 +1,11 @@
+import AA
+import BA
+import CA
+
+import AB
+import BB
+import CB
+
+import AC
+import BC
+import CC
