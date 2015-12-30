32b52c3024c7267d89355569050966cacc827988
itchyny <itchyny@hatena.ne.jp>
2015-12-30 11:19:00 +0900

    add tests for qualified import

---
+++ test/basic/qualified.in.hs
@@ -0,0 +1,11 @@
+import CA
+import qualified BA as BA
+import AA
+
+import CB
+import qualified AB as AB
+import qualified BB as BB
+
+import qualified CC as CC
+import qualified AC as AC
+import BC
---
+++ test/basic/qualified.out.hs
@@ -0,0 +1,11 @@
+import AA
+import qualified BA as BA
+import CA
+
+import qualified AB as AB
+import qualified BB as BB
+import CB
+
+import qualified AC as AC
+import BC
+import qualified CC as CC
